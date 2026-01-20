ea58c611822a6ecbc4201f7a0a80a6ccdbce17db
HartFinne <167497706+HartFinne@users.noreply.github.com>
2026-01-20 13:37:47 +0800

    epic-2: Added toast in the create task

--- src/api/fetchTasks.js
+++ src/api/fetchTasks.js
@@ -1,18 +1,13 @@
 export async function fetchTasks(token, limit, lastUid = null) {
-  try {
-    let url = `http://localhost:8080/tasks?limit=${limit}`
-    if (lastUid) url += `&lastUid=${lastUid}`
+  let url = `http://localhost:8080/tasks?limit=${limit}`
+  if (lastUid) url += `&lastUid=${lastUid}`
 
-    const res = await fetch(url, {
-      headers: { "Authorization": `Bearer ${token}` }
-    });
+  const res = await fetch(url, {
+    headers: { "Authorization": `Bearer ${token}` }
+  });
 
-    if (!res.ok) throw new Error("Failed to fetch tasks");
+  if (!res.ok) throw new Error("Failed to fetch tasks");
 
-    const data = await res.json();
-    return data; // returns { users, lastUid }
-  } catch (error) {
-    console.error("fetchUsers error:", error.message);
-    return { users: [], lastUid: null };
-  }
+  const data = await res.json();
+  return data; // returns { users, lastUid }
 }
--- src/api/fetchUsers.js
+++ src/api/fetchUsers.js
@@ -1,19 +1,12 @@
-
 export async function fetchUsers(token, limit, lastUid = null) {
-  try {
-    let url = `http://localhost:8080/users/users?limit=${limit}`;
-    if (lastUid) url += `&lastUid=${lastUid}`;
+  let url = `http://localhost:8080/users/users?limit=${limit}`;
+  if (lastUid) url += `&lastUid=${lastUid}`;
 
-    const res = await fetch(url, {
-      headers: { "Authorization": `Bearer ${token}` }
-    });
+  const res = await fetch(url, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 
-    if (!res.ok) throw new Error("Failed to fetch users");
+  if (!res.ok) throw new Error("Failed to fetch users");
 
-    const data = await res.json();
-    return data; // returns { users, lastUid }
-  } catch (error) {
-    console.error("fetchUsers error:", error.message);
-    return { users: [], lastUid: null };
-  }
+  return await res.json(); // { users, lastUid }
 }
